d54e8143bac5d02a55e0bbc24af226bf071d3fb3
YYLee26088 <26088@naver.com>
2020-05-04 11:00:54 +0900

    중간커밋6

--- exam/mid3.js
+++ exam/mid3.js
@@ -2,8 +2,14 @@ let person1=[];
 
 let age=16;
 for(let i=0;i<3;i++){
-    let person={name:'홍길동',age:this.age}
+    let person={name:'홍길동',age:age}
     person1.push(person)
     age++;
 }
 
+let person2=person1
+
+person2[0].age=20
+
+console.log(person1)
+console.log(person2)
